36bb521fe218cfca2309796512118dfd6ef30a75
sabotzs <georgi.kuklev@protonmail.com>
2025-09-22 15:54:59 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "visgraph-swift",
+    targets: [
+        .executableTarget(
+            name: "visgraph-swift"
+        ),
+    ]
+)
---
+++ Sources/visgraph-swift/visgraph_swift.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct visgraph_swift {
+    static func main() {
+        print("Hello, world!")
+    }
+}
