728eafb8efcdb44b141f79e164ff78c83eeac052
sabotzs <georgi.kuklev@protonmail.com>
2025-09-22 16:42:04 +0300

    Create index.html and add dependancy to JavaScriptKit.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "visgraph-swift",
+    dependencies: [
+        .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", exact: .init(stringLiteral: "0.36.0")),
+    ],
     targets: [
         .executableTarget(
-            name: "visgraph-swift"
+            name: "visgraph-swift",
+            dependencies: [
+                .product(name: "JavaScriptKit", package: "JavaScriptKit"),
+            ]
         ),
     ]
 )
--- Sources/visgraph-swift/visgraph_swift.swift
+++ Sources/visgraph-swift/visgraph_swift.swift
@@ -1,6 +1,11 @@
+import JavaScriptKit
+
 @main
 struct visgraph_swift {
     static func main() {
-        print("Hello, world!")
+        let document = JSObject.global.document
+        let div = document.createElement("div")
+        div.innerText = "Hello from Swift!"
+        let _ = document.body.appendChild(div)
     }
 }
